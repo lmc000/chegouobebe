6bfad43b29dffba18f24af89a54214019e605ff9
lmc000 <mcostatti@gmail.com>
2026-05-27 15:10:55 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "@/pages/Landing";
+import Blog from "@/pages/Blog";
+import Artigo from "@/pages/Artigo";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<Artigo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
