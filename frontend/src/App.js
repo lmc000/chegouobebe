dd89559f8f54db7a39bc57609eb858d37966b708
lmc000 <mcostatti@gmail.com>
2026-06-02 10:11:35 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,22 @@ import { Analytics } from "@vercel/analytics/react";
 import Landing from "@/pages/Landing";
 import Blog from "@/pages/Blog";
 import Artigo from "@/pages/Artigo";
+import Privacidade from "@/pages/Privacidade";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<Artigo />} />
-        </Routes>
-      </BrowserRouter>
-      <Analytics />
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/blog/:slug" element={<Artigo />} />
+                    <Route path="/privacidade" element={<Privacidade />} />
+                </Routes>
+            </BrowserRouter>
+            <Analytics />
+        </div>
+    );
 }
 
 export default App;
